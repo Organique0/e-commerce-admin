7c981f1380b38b1d3d22d3795432f482787d9fd4
Organique0 <grabnar.luka@gmail.com>
2023-07-13 00:34:07 +0200

    set shipping fee

--- pages/settings.js
+++ pages/settings.js
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import Spinner from "@/components/Spinner";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
@@ -6,43 +7,86 @@ import Swal from "sweetalert2";
 function SettingsPage({ swal }) {
   const [products, setProducts] = useState([]);
   const [featuredProductId, setFeaturedProductId] = useState(null);
+  const [shippingFee, setShippingFee] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
-      setProducts(response.data);
-    });
-    axios.get("/api/settings?name=featuredProductId").then((response) => {
-      setFeaturedProductId(response.data.value);
+    setIsLoading(true);
+    fetchData().then(() => {
+      setIsLoading(false);
     });
   }, []);
 
-  function change(e) {
+  async function fetchData() {
+    await axios.get("/api/products").then((response) => {
+      setProducts(response.data);
+    });
+    await axios.get("/api/settings?name=featuredProductId").then((response) => {
+      setFeaturedProductId(response.data.value);
+    });
+    await axios.get("/api/settings?name=shippingFee").then((response) => {
+      setShippingFee(response.data.value);
+    });
+  }
+
+  function changeFeaturedProduct(e) {
     const newFeaturedProductId = e.target.value;
     setFeaturedProductId(newFeaturedProductId);
   }
 
-  useEffect(() => {
-    saveSettings();
-  }, [featuredProductId]);
+  function changeShippingFee(e) {
+    const newShippingFee = e.target.value;
+    setShippingFee(newShippingFee);
+  }
 
-  function saveSettings() {
-    axios.put("/api/settings", {
+  async function saveSettings() {
+    await axios.put("/api/settings", {
       name: "featuredProductId",
       value: featuredProductId
+    });
+    await axios.put("/api/settings", {
+      name: "shippingFee",
+      value: shippingFee
+    });
+    await swal.fire({
+      title: "Saved",
+      icon: "success"
     });
   }
 
   return (
     <Layout>
       <h1>Your store settings</h1>
-      <label>Featured product</label>
-      <>
-        <select onChange={(e) => change(e)} value={featuredProductId}>
-          {products.length > 0 &&
-            products.map((p) => <option value={p._id}>{p.title}</option>)}
-        </select>
-      </>
-      <div></div>
+      {isLoading ? (
+        <Spinner></Spinner>
+      ) : (
+        <div className="w-6/12">
+          <label>Featured product</label>
+          <br />
+          <select
+            onChange={(e) => changeFeaturedProduct(e)}
+            value={featuredProductId}
+            className="focus:outline-none focus:ring-1 focus:ring-primary"
+          >
+            {products.length > 0 &&
+              products.map((p) => <option value={p._id}>{p.title}</option>)}
+          </select>
+          <br />
+          <label>Shipping cost</label>
+          <br />
+          <input
+            type="number"
+            className="w-2/12 focus:outline-none focus:ring-1 focus:ring-primary"
+            value={shippingFee}
+            onChange={(e) => changeShippingFee(e)}
+          ></input>
+          <label className=""> EVRO</label>
+          <br />
+          <button className="btn-primary" onClick={saveSettings}>
+            save
+          </button>
+        </div>
+      )}
     </Layout>
   );
 }
@@ -50,9 +94,7 @@ function SettingsPage({ swal }) {
 const swalWithCustomStyle = Swal.mixin({
   customClass: {
     title: "font-semibold text-3xl",
-    confirmButton: "btn-red",
-    cancelButton: "btn-default mx-1"
-  },
-  buttonsStyling: false
+    confirmButton: "btn-primary mx-1"
+  }
 });
 export default () => <SettingsPage swal={swalWithCustomStyle} />;
